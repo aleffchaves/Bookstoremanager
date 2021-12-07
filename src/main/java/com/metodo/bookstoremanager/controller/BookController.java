b9b6fe29dac419edac6869710086b3a1021a65bc
Alef Meira <alef.chaves01@gmail.com>
2021-12-07 11:27:58 -0300

    firt commit - project Bookstoremanager started

---
+++ src/main/java/com/metodo/bookstoremanager/controller/BookController.java
@@ -0,0 +1,16 @@
+package com.metodo.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping()
+    public String hello(){
+        return "Hello BookstoreManager";
+    }
+
+}
