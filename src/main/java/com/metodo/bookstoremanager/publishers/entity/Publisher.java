9859c38a94507a3882db7e79b0a25589ef2b87a1
Alef Meira <alef.chaves01@gmail.com>
2021-12-08 16:45:40 -0300

    Created auditable entity

--- src/main/java/com/metodo/bookstoremanager/publishers/entity/Publisher.java
+++ src/main/java/com/metodo/bookstoremanager/publishers/entity/Publisher.java
@@ -1,6 +1,7 @@
 package com.metodo.bookstoremanager.publishers.entity;
 
 import com.metodo.bookstoremanager.books.entity.Book;
+import com.metodo.bookstoremanager.entity.Auditable;
 import lombok.Data;
 
 import javax.persistence.*;
@@ -9,7 +10,7 @@ import java.util.List;
 
 @Data
 @Entity
-public class Publisher {
+public class Publisher extends Auditable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
