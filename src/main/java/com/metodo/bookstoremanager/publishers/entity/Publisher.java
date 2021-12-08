498b9f30fcbc4f9e5eec8ef1c8c556830244a4d8
Alef Meira <alef.chaves01@gmail.com>
2021-12-08 16:21:27 -0300

    Created models mappings

---
+++ src/main/java/com/metodo/bookstoremanager/publishers/entity/Publisher.java
@@ -0,0 +1,30 @@
+package com.metodo.bookstoremanager.publishers.entity;
+
+import com.metodo.bookstoremanager.books.entity.Book;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String code;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate foundationDate;
+
+    @OneToMany(mappedBy = "publisher",fetch = FetchType.LAZY)
+    private List<Book> books;
+
+}
---
+++ src/main/java/com/metodo/bookstoremanager/users/entity/User.java
@@ -0,0 +1,44 @@
+package com.metodo.bookstoremanager.users.entity;
+
+import com.metodo.bookstoremanager.books.entity.Book;
+import com.metodo.bookstoremanager.users.enums.Gender;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Gender gender;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate birthdate;
+
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY)
+    private List<Book> books;
+
+}
