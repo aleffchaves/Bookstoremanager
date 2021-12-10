d41de891544bec7e14d13f9c483bcaaeb3a8320e
Alef Meira <alef.chaves01@gmail.com>
2021-12-10 16:54:07 -0300

    Created test structure class

---
+++ src/test/java/com/metodo/bookstoremanager/author/service/AuthorServiceTest.java
@@ -0,0 +1,29 @@
+package com.metodo.bookstoremanager.author.service;
+
+import com.metodo.bookstoremanager.author.builder.AuthorDTOBuilder;
+import com.metodo.bookstoremanager.author.mapper.AuthorMapper;
+import com.metodo.bookstoremanager.author.repository.AuthorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorServiceTest {
+
+    private final AuthorMapper authorMapper = AuthorMapper.INSTANCE;
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorService authorService;
+
+    private AuthorDTOBuilder authorDTOBuilder;
+
+    @BeforeEach
+    void setUp(){
+        authorDTOBuilder = AuthorDTOBuilder.builder().build();
+    }
+}
