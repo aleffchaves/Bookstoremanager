c2e3fb39adec269c7f27f8059a7cba3e92c938f7
Alef Meira <alef.chaves01@gmail.com>
2021-12-13 11:32:30 -0300

    Added creation methods in the services layer and their respcteive tests

--- src/main/java/com/metodo/bookstoremanager/author/repository/AuthorRepository.java
+++ src/main/java/com/metodo/bookstoremanager/author/repository/AuthorRepository.java
@@ -3,5 +3,9 @@ package com.metodo.bookstoremanager.author.repository;
 import com.metodo.bookstoremanager.author.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Optional<Author> findByName(String name);
 }
