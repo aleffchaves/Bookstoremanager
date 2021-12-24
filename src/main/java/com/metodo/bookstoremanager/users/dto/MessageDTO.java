cb9bce4c1e49cee8bb53d5ab61fe25f86a2c8613
alefmeira <alef.chaves01@gmail.com>
2021-12-24 14:25:33 -0300

    created message dto for users operation return message

---
+++ src/main/java/com/metodo/bookstoremanager/users/dto/MessageDTO.java
@@ -0,0 +1,14 @@
+package com.metodo.bookstoremanager.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class MessageDTO {
+    private String message;
+}
