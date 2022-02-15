0f2673550e99050265b1b8de44d6a2ec63d4c63e
alefmeira <alef.chaves01@gmail.com>
2022-02-14 21:44:45 -0300

    Developed authentication entry point for exception handling

---
+++ src/main/java/com/metodo/bookstoremanager/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,17 @@
+package com.metodo.bookstoremanager.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationEntryPoint  implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException exception) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "UNAUTHORIZED");
+    }
+}
