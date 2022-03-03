f154f017f04d6b17e811d1dc502888578ac94832
alefmeira <alef.chaves01@gmail.com>
2022-03-03 09:02:01 -0300

    developed books creation feature

--- src/main/java/com/metodo/bookstoremanager/books/controller/BookController.java
+++ src/main/java/com/metodo/bookstoremanager/books/controller/BookController.java
@@ -1,13 +1,18 @@
 package com.metodo.bookstoremanager.books.controller;
 
+import com.metodo.bookstoremanager.books.dto.BookRequestDTO;
+import com.metodo.bookstoremanager.books.dto.BookResponseDTO;
 import com.metodo.bookstoremanager.books.service.BookService;
+import com.metodo.bookstoremanager.users.dto.AuthenticatedUser;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(path = "/api/v1/books")
@@ -20,5 +25,11 @@ public class BookController implements BookControllerDocs{
         this.bookService = bookService;
     }
 
-
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookResponseDTO create(
+            @AuthenticationPrincipal AuthenticatedUser authenticatedUser,
+            @RequestBody @Valid BookRequestDTO bookRequestDTO) {
+        return bookService.create(authenticatedUser, bookRequestDTO);
+    }
 }
--- src/test/java/com/metodo/bookstoremanager/books/Controller/BookControllerTest.java
+++ src/test/java/com/metodo/bookstoremanager/books/Controller/BookControllerTest.java
@@ -3,16 +3,30 @@ package com.metodo.bookstoremanager.books.Controller;
 import com.metodo.bookstoremanager.books.builder.BookRequestDTOBuilder;
 import com.metodo.bookstoremanager.books.builder.BookResponseDTOBuilder;
 import com.metodo.bookstoremanager.books.controller.BookController;
+import com.metodo.bookstoremanager.books.dto.BookRequestDTO;
+import com.metodo.bookstoremanager.books.dto.BookResponseDTO;
 import com.metodo.bookstoremanager.books.service.BookService;
+import com.metodo.bookstoremanager.users.dto.AuthenticatedUser;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
+
+import static com.metodo.bookstoremanager.utils.JsonConversionUtils.asJsonString;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
 public class BookControllerTest {
@@ -40,5 +54,34 @@ public class BookControllerTest {
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver())
                 .setViewResolvers((s, locale) -> new MappingJackson2JsonView())
                 .build();
+    }
+
+    @Test
+    void whenPOSTIsCalledThenCreatedStatusShouldBeReturned() throws Exception {
+        BookRequestDTO expectedBookToCreateDTO = bookRequestDTOBuilder.buildRequestBookDTO();
+        BookResponseDTO expectedCreatedBookDTO = bookResponseDTOBuilder.buildResponseBookDTO();
+
+        when(bookService.create(any(AuthenticatedUser.class), eq(expectedBookToCreateDTO)))
+                .thenReturn(expectedCreatedBookDTO);
+
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BOOKS_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(expectedBookToCreateDTO)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id", is(expectedCreatedBookDTO.getId().intValue())))
+                .andExpect(jsonPath("$.id", is(expectedCreatedBookDTO.getId().intValue())))
+                .andExpect(jsonPath("$.id", is(expectedCreatedBookDTO.getId().intValue())));
+    }
+
+    @Test
+    void whenPOSTIsCalledWithOutRequiredFieldThenBadRequestStatusShouldBeReturned () throws Exception {
+        BookRequestDTO expectedBookToCreateDTO = bookRequestDTOBuilder.buildRequestBookDTO();
+        expectedBookToCreateDTO.setIsbn(null);
+
+        mockMvc.perform(MockMvcRequestBuilders.post(BOOKS_API_URL_PATH)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(expectedBookToCreateDTO)))
+                        .andExpect(status().isBadRequest());
     }
 }
